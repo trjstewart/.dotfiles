c44dfd5bf15896020fb6528d5522b6d6534b2154
Tylor Stewart <trjstewart@gmail.com>
2023-12-19 21:56:41 +1000

    bare minimum bootstrap

--- bootstrap-helpers.sh
+++ bootstrap-helpers.sh
@@ -53,13 +53,152 @@ background_light_magenta='\e[105m'
 background_light_cyan='\e[106m'
 background_white='\e[107m'
 
-print_with_newline() { printf '%b\n' "$*"; }
-print_with_fixed_width() { printf '%-100s' "$*"; }
+print_with_new_line() { printf '%b\n' "$*"; }
+print_with_no_new_line() { printf '%b' "$*"; }
+print_with_fixed_width() { printf '%-75b' "$*"; }
 
-print_success() { print_with_newline "${foreground_green}$*${format_reset_all}"; }
-print_message() { print_with_newline "${foreground_cyan}$*${format_reset_all}"; }
-print_info() { print_with_newline "${foreground_magenta}$*${format_reset_all}"; }
-print_warning() { print_with_newline "${foreground_yellow}$*${format_reset_all}"; }
-print_error() { print_with_newline "${foreground_red}$*${format_reset_all}"; }
+print_success() { print_with_new_line "${foreground_green}$*${format_reset_all}"; }
+print_message() { print_with_new_line "${foreground_cyan}$*${format_reset_all}"; }
+print_info() { print_with_new_line "${foreground_magenta}$*${format_reset_all}"; }
+print_warning() { print_with_new_line "${foreground_yellow}$*${format_reset_all}"; }
+print_error() { print_with_new_line "${foreground_red}$*${format_reset_all}"; }
 
-[[ "${BASH_SOURCE[0]}" != "${0}" ]] && print_info ">>> ${BASH_SOURCE[0]#"./"} has been sourced..."
+print_success_no_new_line() { print_with_no_new_line "${foreground_green}$*${format_reset_all}"; }
+print_message_no_new_line() { print_with_no_new_line "${foreground_cyan}$*${format_reset_all}"; }
+print_info_no_new_line() { print_with_no_new_line "${foreground_magenta}$*${format_reset_all}"; }
+print_warning_no_new_line() { print_with_no_new_line "${foreground_yellow}$*${format_reset_all}"; }
+print_error_no_new_line() { print_with_no_new_line "${foreground_red}$*${format_reset_all}"; }
+
+print_success_fixed_width() { print_with_fixed_width "${foreground_green}$*${format_reset_all}"; }
+print_message_fixed_width() { print_with_fixed_width "${foreground_cyan}$*${format_reset_all}"; }
+print_info_fixed_width() { print_with_fixed_width "${foreground_magenta}$*${format_reset_all}"; }
+print_warning_fixed_width() { print_with_fixed_width "${foreground_yellow}$*${format_reset_all}"; }
+print_error_fixed_width() { print_with_fixed_width "${foreground_red}$*${format_reset_all}"; }
+
+print_error_and_exit() { print_error "$*\n"; exit 1; }
+request_sudo_access() { if ! sudo -v; then print_error_and_exit "You must have sudo access to run this script!"; fi }
+silent() { "$@" > /dev/null  2>&1; }
+get_system_os() { uname -s; }
+is_mac() { if [[ "$(get_system_os)" == "Darwin" ]]; then return 0; else return 1; fi }
+is_linux() { if [[ "$(get_system_os)" == "Linux" ]]; then return 0; else return 1; fi }
+is_linux_ubuntu() { if [[ "$(lsb_release -i | tr -d '\t' | grep -Po '(?<=:).*')" == "Ubuntu" ]]; then return 0; else return 1; fi }
+is_wsl() { if grep -q microsoft /proc/version; then return 0; else return 1; fi }
+
+verify_location() {
+  if [[ ! "${PWD##*/}" == ".dotfiles" ]]; then print_error_and_exit "You must be in the .dotfiles folder to run the bootstrap script!"; fi
+  if [[ ! $(dirname "$PWD") == "$HOME" ]]; then print_error_and_exit "The .dotfiles folder must be located inside your home directory!"; fi
+}
+
+print_last_command_success_or_failure() {
+  LAST_STATUS_CODE=$?
+
+  if [ $# -eq 0 ]; then
+    # if there is no arguments, then we're just checking the last command's exit code
+    if [ $LAST_STATUS_CODE -eq 0 ]; then print_success "✔"; else print_error "✗"; fi
+  else
+    # if there are arguments, then we're checking the last command's exit code against any of the arguments
+    for SUCCESS_STATUS in "$@"; do
+      if [ $LAST_STATUS_CODE -eq "$SUCCESS_STATUS" ]; then print_success "✔"; return; fi
+    done
+
+    # if we get here, then we didn't find a matching exit code
+    print_error "✗"
+  fi
+}
+
+apt_get_update() {
+  UNIX_TIME=$(date +%s)
+  LAST_UPDATED=$(stat --format="%X" /var/cache/apt/pkgcache.bin)
+  TIME_DIFF=$((UNIX_TIME - LAST_UPDATED))
+
+  if [[ "${TIME_DIFF}" -gt 3600 ]]; then
+    sudo apt-get update -y > "$LOG_PREFIX-apt-get-update.log" 2>&1
+    print_last_command_success_or_failure
+  else
+    print_success "✔"
+    print_warning "    ! apt was already updated in the past hour"
+  fi
+}
+
+install_brew() { # TODO: This could do with some improvement. It definitely doesn't handle errors well.
+  print_message_fixed_width " >> Installing Brew..."
+  if ! silent command -v "brew"; then
+    export CI=1
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > "$LOG_PREFIX-brew-install.log" 2>&1
+    print_last_command_success_or_failure
+    unset CI
+
+    # post install steps
+    (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/elliot/.bashrc
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  else
+    brew update > "$LOG_PREFIX-brew-update.log" 2>&1
+    print_last_command_success_or_failure
+    print_warning "    ! Brew is already installed, updated instead...         "
+  fi
+}
+
+determine_personal_or_work() {
+  if is_wsl; then
+    DOMAIN=$(powershell.exe '(Get-WmiObject win32_computersystem).Domain' | tr -d '\r')
+    case $DOMAIN in
+      ndia.gov.au)
+        echo "work_ndia"
+        return 0
+        ;;
+      ndis.gov.au)
+        echo "work_ndia"
+        return 0
+        ;;
+    esac
+  fi
+
+  echo "personal"
+  return 0
+}
+
+get_email() {
+  case $(determine_personal_or_work) in
+    work_ndia)
+      echo "tylor.stewart@ndis.gov.au"
+      return 0
+      ;;
+    *)
+      echo "trjstewart@gmail.com"
+      return 0
+      ;;
+  esac
+}
+
+create_gitconfig() {
+  GIT_CONFIG_PATH="./git"
+  GIT_CONFIG_TEMPLATE_DIRECTORY="templates"
+  GIT_CONFIG_TEMPLATE_FILE=".gitconfig"
+
+  if [[ ! -f "$GIT_CONFIG_TEMPLATE_DIRECTORY/$GIT_CONFIG_TEMPLATE_FILE" ]]; then print_error_and_exit "Couldn't find the .gitconfig template file!"; fi
+  if [ $# -eq 0 ]; then print_error_and_exit "No argument supplied. You must supply your email address!"; fi
+  if [ -z "$1" ]; then print_error_and_exit "Invalid argument supplied. You must supply your email address!"; fi
+
+  print_message_fixed_width " >> Copying template .gitconfig file..."
+  cp "$GIT_CONFIG_TEMPLATE_DIRECTORY/$GIT_CONFIG_TEMPLATE_FILE" "$GIT_CONFIG_PATH/.gitconfig"
+  print_last_command_success_or_failure
+
+  print_message_fixed_width " >> Replacing email template value in .gitconfig file..."
+  if is_mac; then
+    silent sed -i "" -e "s/__EMAIL_ADDRESS__/$1/" "$GIT_CONFIG_PATH/.gitconfig"
+  else
+    silent sed -i -e "s/__EMAIL_ADDRESS__/$1/" "$GIT_CONFIG_PATH/.gitconfig"
+  fi
+  print_last_command_success_or_failure
+}
+
+unpack_with_stow() {
+  for point in $(echo */); do
+    if [[ "$point" == "$LOG_DIRECTORY/" || "$point" == "templates/" ]]; then continue; fi
+    print_message_fixed_width " >> Linking ${point/\//}..."
+    silent stow $point # for some reason logfile creation fails if we try to direct output to a file
+    print_last_command_success_or_failure
+  done
+}
+
+[[ "${BASH_SOURCE[0]}" != "${0}" ]] && print_info ">>> ${BASH_SOURCE[0]#"./"} has been sourced...\n"
--- bootstrap.sh
+++ bootstrap.sh
@@ -7,13 +7,65 @@ DEBUG=false
 # Clear the screen so we can see the script run nicely
 clear
 
-# We haven't pulled in our helpers quite yet but there are a few potential errors we need to handle
-error_message_and_close() { printf '%b\n' "\e[31m$*\e[0m"; exit 1; }
+# Ensure the script is being run from the right directory and execute the helpers
+error_message_and_close() { printf '%b\n\n' "\e[31m$*\e[0m"; exit 1; }
+if [[ ! "${PWD##*/}" == ".dotfiles" ]]; then error_message_and_close "You must be in the .dotfiles folder to run the bootstrap script!"; fi
+if [[ -e ./bootstrap-helpers.sh ]]; then . ./bootstrap-helpers.sh; else error_message_and_close "Couldn't load bootstrap-helpers (this should never actually happen)"; fi
 
-# verify we're running the bootstrap script from within the .dotfiles directory
-if [[ ! "${PWD##*/}" == ".dotfiles" ]]; then
-  error_message_and_close "You must be in the .dotfiles folder to run the bootstrap script!"
+
+print_message "Welcome to bootstrapper $USER! Let's get started setting up your system... 🤔"
+verify_location
+request_sudo_access
+mkdir -p $LOG_DIRECTORY
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Bootstrapping that's specific to each platform
+# ---------------------------------------------------------------------------------------------------------------------
+if is_mac; then
+  print_message "It looks like you're trying to setup a mac, let's get brewing!"
 fi
 
-# import some helper functions to make our life a little easier
-if [[ -e ./bootstrap-helpers.sh ]]; then . ./bootstrap-helpers.sh; else error_message_and_close "Couldn't load bootstrap-helpers (this should never actually happen)"; fi
+if is_linux; then
+  print_message_no_new_line "It looks like you're trying to setup linux, but which flavour?"
+
+  if is_linux_ubuntu; then
+    print_info " It's Ubuntu, let's apt-get going!"
+
+    print_info "\nInstall System Updates"
+    print_message_fixed_width " >> Updating package information..."
+    apt_get_update
+    print_message_fixed_width " >> Updating existing packages..."
+    sudo apt-get dist-upgrade -y > "$LOG_PREFIX-apt-get-dist-upgrade.log" 2>&1; print_last_command_success_or_failure
+
+    print_info "\nInstall Required Packages from APT"
+    print_message_fixed_width " >> Installing prerequisites to use Brew..."
+    sudo apt-get install -y build-essential procps curl file git > "$LOG_PREFIX-apt-get-install.log" 2>&1; print_last_command_success_or_failure
+    print_message_fixed_width " >> Cleaning up unused packages..."
+    sudo apt-get autoremove > "$LOG_PREFIX-apt-get-autoremove.log" 2>&1; print_last_command_success_or_failure
+    print_message_fixed_width " >> Cleaning up unused cache files..."
+    sudo apt-get autoclean > "$LOG_PREFIX-apt-get-autoclean.log" 2>&1; print_last_command_success_or_failure
+  fi
+fi
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Bootstrapping that's common to all platforms
+# ---------------------------------------------------------------------------------------------------------------------
+print_info "\nGet Setup Using Brew 🍺"
+install_brew
+print_message_fixed_width " >> Installing everyting in the Brewfile..."
+brew bundle > "$LOG_PREFIX-brew-bundle.log" 2>&1; print_last_command_success_or_failure
+
+print_info "\nSetting up Git Config"
+create_gitconfig "$(get_email)"
+
+print_info "\nLink dotfiles with stow"
+unpack_with_stow
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Job's Done!
+# ---------------------------------------------------------------------------------------------------------------------
+print_success "\nBootstrap Complete! 🎉\n"
+
+# disable debug logging if set
+[ "$DEBUG" == 'true' ] && set +x
