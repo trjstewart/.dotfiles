9c649086a42acb4b05dd056cabd38cf9ace2768c
Tylor Stewart <tylor.stewart@ndis.gov.au>
2025-06-13 14:08:17 +1000

    add terraform helper function to assist with cross-platform providers

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -8,3 +8,11 @@ what_is_my_ip() {
 zsh.update_plugins() {
 
 }
+
+terraform() {
+  if [[ "$1" == "init" && "$2" == "-upgrade" && $# -eq 2 ]]; then
+    command terraform init -upgrade && terraform providers lock -platform=linux_amd64 -platform=darwin_amd64
+  else
+    command terraform "$@"
+  fi
+}
